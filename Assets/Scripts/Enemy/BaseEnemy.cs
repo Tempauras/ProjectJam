89e9e58c622edb10c7a14ad9328c286b66db9ebb
Arthur Bouffay <80390248+Tempauras@users.noreply.github.com>
2022-04-09 02:25:31 +0200

    A* setup + moving ai

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour
 {
     //Attribute
+
+    #region AI stuff
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _nextWaypointDistance = 3.0f;
+
+    private Path _path;
+    private int _currentWaypoint = 0;
+    private bool _reachedEndOfPath = false;
+
+    private Seeker _seeker;
+    private Rigidbody2D _rb;
+    #endregion
+    
+    
     [SerializeField] private float _health;
 
     [SerializeField] private float _damage;
 
-    [SerializeField] private float _moveSpeed;
+    
 
     [SerializeField] private float _attackSpeed;
 
@@ -20,13 +36,46 @@ public class BaseEnemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _agent = GetComponent<NavMeshAgent>();
-        _agent.speed = _moveSpeed;
+        _seeker = GetComponent<Seeker>();
+        _rb = GetComponent<Rigidbody2D>();
+
+        _seeker.StartPath(_rb.position, _target.position, OnPathComplete);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_path == null)
+            return;
+
+        if (_currentWaypoint >= _path.vectorPath.Count)
+        {
+            _reachedEndOfPath = true;
+            return;
+        } else
+        {
+            _reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2) _path.vectorPath[_currentWaypoint] - _rb.position).normalized;
+        Vector2 force = direction * (_moveSpeed * Time.deltaTime);
+
+        _rb.AddForce(force);
         
+        float distance = Vector2.Distance(_rb.position, _path.vectorPath[_currentWaypoint]);
+
+        if (distance < _nextWaypointDistance)
+        {
+            _currentWaypoint++;
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            _path = p;
+            _currentWaypoint = 0;
+        }
     }
 }
---
+++ Assets/Scripts/Enemy/EnemyGFX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGFX : MonoBehaviour
+{
+    [SerializeField] private AIPath _aiPath;
+    // Update is called once per frame
+    void Update()
+    {
+        if (_aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (_aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}

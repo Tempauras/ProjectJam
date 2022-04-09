c056b0cc997693aae0be06f1e9404e37228105e7
Arthur Bouffay <80390248+Tempauras@users.noreply.github.com>
2022-04-09 04:31:19 +0200

    Cleanup enemy

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -6,25 +6,28 @@ using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour
 {
+    public GameObject firePoint;
     #region AI stuff
     public Transform target;
     private Seeker _seeker;
     private Rigidbody2D _rb;
+    private AIPath _aiPath;
     #endregion
     [SerializeField] private Object _bullet;
     [SerializeField] private float _lifePoints;
     [SerializeField] private float _damage;
     [SerializeField] private float _attackDelay;
     [SerializeField] private float _range;
-
-
     private float _nextAttack = 0.1f;
+    
+    
     
     // Start is called before the first frame update
     void Start()
     {
         _seeker = GetComponent<Seeker>();
         _rb = GetComponent<Rigidbody2D>();
+        _aiPath = GetComponent<AIPath>();
     }
 
     // Update is called once per frame
@@ -36,15 +39,24 @@ public class BaseEnemy : MonoBehaviour
         {
             Attack();
         }
+        
+        if (_aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (_aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 
     public void Attack()
     {
         Debug.Log("Attacking at : " + target.localPosition.x + ", " + target.localPosition.y + " !");
+        Vector2 destination = target.position;
+        
         GameObject projectileGameObject = (GameObject)Instantiate(_bullet, transform.position, transform.rotation);
         BulletBhaviour bullet = projectileGameObject.GetComponent<BulletBhaviour>();
         
-        Vector2 destination = target.position;
         Vector2 direction =  destination - new Vector2(transform.position.x, transform.position.y);
         bullet.SetDirection(direction.normalized);
         float lookAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;

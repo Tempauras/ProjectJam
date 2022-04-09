29ed531903d6b7da41d942f696f1cf4f8f0e8f3c
Elvyn <elvynwakeford@gmail.com>
2022-04-09 04:08:03 +0200

    shoot mechanics

---
+++ Assets/Scripts/BulletBhaviour.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBhaviour : MonoBehaviour
+{
+
+    public int damage;
+    public int force;
+    
+    public int travelDistance;
+
+    private Rigidbody2D _rigidbody2D;
+    
+    private Vector2 direction;
+    private Vector3 StartPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        StartPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody2D.velocity = direction * force;
+
+        if ((transform.position - StartPos).magnitude > travelDistance)
+        {
+            Destroy(gameObject);
+        }
+        
+        
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        BaseEnemy enemy = col.GetComponent<BaseEnemy>();
+
+        if (enemy)
+        {
+            enemy.Hit(damage);
+        }
+        
+    }
+
+    public void SetDirection(Vector2 prmDirection)
+    {
+        direction = prmDirection;
+    }
+}
--- Assets/Scripts/Player/PlayerBehviour.cs
+++ Assets/Scripts/Player/PlayerBehviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +11,11 @@ public class PlayerBehviour : MonoBehaviour
     
     private Rigidbody2D _rigidbody;
     private Vector2 _xzAxis;
+
+    public Camera camera;
+    public Object projectile;
+    public int lifePoints;
+    public int maxLifePoints;
     
     // Start is called before the first frame update
     void Start()
@@ -22,8 +28,6 @@ public class PlayerBehviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_xzAxis.magnitude);
-        
         if (_xzAxis.magnitude != 0)
         {
             _rigidbody.velocity = _xzAxis * moveSpeed;
@@ -39,13 +43,39 @@ public class PlayerBehviour : MonoBehaviour
 
     public void OnMovement(InputValue prmInputValue)
     {
-        Debug.Log("XZAXIS start");
-
         _xzAxis = prmInputValue.Get<Vector2>();
-
-        Debug.Log("XZAXIS end");
-        
-        
-        
     }
+
+    public void OnShoot()
+    {
+        GameObject projectileGameObject = (GameObject)Instantiate(projectile, transform.position, transform.rotation);
+        BulletBhaviour bullet = projectileGameObject.GetComponent<BulletBhaviour>();
+
+        Vector2 destination = camera.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 direction =  destination - new Vector2(transform.position.x, transform.position.y);
+        
+        bullet.SetDirection(direction.normalized);
+        
+        float lookAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        
+        bullet.transform.rotation = Quaternion.Euler(0, 0, lookAngle);
+        
+
+    }
+
+    public void Hit(int prmDamage)
+    {
+        lifePoints -= prmDamage;
+        if (lifePoints <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Debug.Log("Death");
+    }
+
 }
